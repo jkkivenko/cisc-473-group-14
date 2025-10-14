2307823c44638c0628531c838764b722983dcb3a
Zachary-Kiz <zachary.kizell@gmail.com>
2025-10-14 14:06:02 -0400

    load images for NN

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello, Trajan!")
+from torch.utils.data import random_split, DataLoader
+from torchvision import datasets, transforms
+
+IMG_FOLDER = "images/all_images"
+IMG_SIZE = 256
+
+transform = transforms.Compose([
+    transforms.Resize((IMG_SIZE,IMG_SIZE)),
+    transforms.ToTensor()
+])
+
+dataset = datasets.ImageFolder(IMG_FOLDER, transform=transform)
+
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+
+train_data, test_data = random_split(dataset, [train_size, test_size])
+
+train_loader = DataLoader(train_data, batch_size=8)
+test_loader = DataLoader(test_data, batch_size=8)
+
+
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# input_tensor = input_tensor.to(device)
+
+
+# print("Hello, Trajan!")
