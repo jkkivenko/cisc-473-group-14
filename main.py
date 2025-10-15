e8628411477c434515aecc10a8bf676b192d27d5
jkkiveko <jameskk2004@gmail.com>
2025-10-14 20:02:24 -0400

    Add PainterNetwork file

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from torch.utils.data import random_split, DataLoader
 from torchvision import datasets, transforms
+import torch
 
-IMG_FOLDER = "images/all_images"
+from network import PainterNetwork
+
+print("Hello, Trajan!")
+
+IMG_FOLDER = "images"
 IMG_SIZE = 256
 
 transform = transforms.Compose([
     transforms.Resize((IMG_SIZE,IMG_SIZE)),
     transforms.ToTensor()
 ])
+
+print("Loading dataset...")
 
 dataset = datasets.ImageFolder(IMG_FOLDER, transform=transform)
 
@@ -19,9 +26,12 @@ train_data, test_data = random_split(dataset, [train_size, test_size])
 train_loader = DataLoader(train_data, batch_size=8)
 test_loader = DataLoader(test_data, batch_size=8)
 
-
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # input_tensor = input_tensor.to(device)
 
+print("Generating model...")
 
-# print("Hello, Trajan!")
+model = PainterNetwork.to(device)
+
+print("Model generated:")
+print(model)
