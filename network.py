f5a67ae0342f414da6fb95bc9cdec50e384db071
123abcpie <trajanthebrown@gmail.com>
2025-10-14 20:45:12 -0400

    Added Conv

--- network.py
+++ network.py
@@ -1,5 +1,5 @@
-import torch
-from torch import nn
+import torch.nn as nn
+import torch.nn.functional as Fimport
 
 # Define model
 class PainterNetwork(nn.Module):
@@ -20,3 +20,27 @@ class PainterNetwork(nn.Module):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
+
+class ConvolutionalNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # self.conv1 = nn.Conv2d(3, 6, 5)
+        # self.pool = nn.MaxPool2d(2, 2)
+        # self.conv2 = nn.Conv2d(6, 16, 5)
+        # self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        # self.fc2 = nn.Linear(120, 84)
+        # self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+
+#https://docs.pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
